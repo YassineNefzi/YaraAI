c45e139d6945b71c4e12a7dfb1f1a4cb38c9c427
YassineNefzi <mohamedyassine.nefzi@insat.ucar.tn>
2024-04-15 16:00:34 +0100

    gitignore issue fixed

--- tests/test_retrieval_qa.py
+++
@@ -1,8 +0,0 @@
-from runnables.chains.retrieval_qa import retrieve_from_pdf
-
-if __name__ == "__main__":
-    
-    while True:
-        query = input("Enter your question: ")
-        answer = retrieve_from_pdf(query)
-        print(answer)
